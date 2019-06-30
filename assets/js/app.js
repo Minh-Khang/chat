05d83972955f26a091833d5dd281a9cdb14721bb
LMK <luongminhkhang@gmail.com>
2019-06-30 12:56:52 +0700

    Create Chatroom

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,4 +14,30 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import socket from "./socket"
+
+let channel = socket.channel("chat_room:lobby", {});
+let list    = $("#message-list");
+let message = $("#msg");
+let name    = $("#name");
+
+message.on("keypress", event => {
+    if (event.keyCode == 13 && message.val() != "") {
+      channel.push("shout", {
+        name: name.val(),
+        message: message.val()
+      });
+      message.val("");
+    }
+});
+
+channel.on("shout", payload => {
+  list.append(`<b>${payload.name || "new_user"}:</b> ${payload.message}<br>`);
+  list.prop({
+    scrollTop: list.prop("scrollHeight")
+  });
+});
+
+channel.join()
+  .receive("ok",    resp => {console.log("Join successfully!", resp);})
+  .receive("error", resp => {console.log("Unable to join!", resp);});
